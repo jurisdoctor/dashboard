60b5bfb2bb43eeb65995381324dd390a78767017
jurisdoctor <tomtldr@gmail.com>
2023-11-23 07:35:15 -0800

    set up authentication and header

---
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -0,0 +1,24 @@
+import NextAuth, { AuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
+"use client";
+
 import Dashboard from "@/app/dashboard";
 import Header from "@/app/components/Header";
-import { Metadata } from "next";
+import Login from "./components/Login";
 import SideMenu from "@/app/components/SideMenu";
 import styles from "./page.module.css";
-
-export const metadata: Metadata = {
-  title: "dashboardt",
-};
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <>
       <main className={styles.main}>
         <Header />
-        <SideMenu />
-        <Dashboard />
+        {session && (
+          <>
+            <SideMenu />
+            <Dashboard />
+          </>
+        )}
+        <Login />
       </main>
     </>
   );
